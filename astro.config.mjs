4e4d0ca9b5ab0c3f905cd0b9faf3071ba02fa8ca
Ashtin Sotelo <ashtinsotelo@gmail.com>
2024-11-26 17:38:51 -0500

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
+  adapter: cloudflare(),
   site: "https://ashtin18.github.io"
 });
