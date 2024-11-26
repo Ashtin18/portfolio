00071af5f879aa025b58f30e2534df4dcea0f39e
Ashtin Sotelo <ashtinsotelo@gmail.com>
2024-11-26 17:37:25 -0500

    change output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  output: "server",
   site: "https://ashtin18.github.io"
 });
